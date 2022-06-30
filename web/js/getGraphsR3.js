d2e53c8ef35958419ece82cdf4b58087e731e2e9
HubashovD <34233868+HubashovD@users.noreply.github.com>
2022-07-01 01:01:31 +0300

    Update getGraphsR3.js

--- web/js/getGraphsR3.js
+++ web/js/getGraphsR3.js
@@ -163,79 +163,6 @@ function getGraphs() {
             list.appendChild(opt)
         })
 
-
-
-        // json.data.forEach(function(d) {
-        //     //console.log(d)
-        //     date = d3.timeParse("%Y")(d.timePeriodStart)
-        //     data.push({
-        //             year: date,
-        //             value: d.value,
-        //             series: d.series,
-        //             seriesDescription: d.seriesDescription
-        //         })
-        //         // для кожного запису в діменшенс кожного запису
-        //     for (const [key, value] of Object.entries(d.dimensions)) {
-
-        //         // спочатку перевіряємо чи є цей ключ у словнику ключів
-        //         // if (listOfKeys.includes(key)) {
-
-        //         // } else { listOfKeys.push(key) }
-
-        //         // для кожного запису в списку із ключами
-        //         // listOfKeys.forEach(function(fromKey) {
-        //         //     // якщо запис у списку ключів співпадає із ключем у запису в таблиці даних 
-
-        //             if (key == fromKey) {
-        //                 console.log(fromKey)
-        //                 console.log(key)
-        //                     // якщо довжина фінального словника = 0 - просто додаємо запис
-        //                 if (dimentionsDict.length == 0) {
-        //                     dimentionsDict.push({
-        //                             name: fromKey,
-        //                             value: value
-        //                         })
-        //                         // в іншому випадку проходимось по всіх записах фінального словника
-        //                 } else {
-        //                     dimentionsDict.forEach(function(dimention) {
-        //                         console.log(dimention)
-        //                             // якщо dimention.name = ключу із таблиці із даними 
-        //                         if (dimention.name == fromKey) {
-        //                             var listOfThings = [] // створюємо порожній список
-        //                                 // якщо у dimention.value список - перебираємо його і додаємо до списку кожне значення, інакше додаємо одне значення
-        //                             try {
-        //                                 dimention.value.forEach(function(singleValue) {
-        //                                     listOfThings.push(singleValue)
-        //                                 })
-        //                             } catch {
-        //                                 listOfThings.push(dimention.value)
-        //                             }
-        //                             // якщо у списку значень вже є значення нічого не робиномо, інакше - записуємо нове значення
-        //                             if (listOfThings.includes(value)) {
-
-        //                             } else { listOfThings.push(value) }
-
-        //                             dimentionsDict = dimentionsDict.filter((item) => typeof(item) === 'object' && item.name !== fromKey); // отфильтровать массив, в котором подходить под условие будут только те объекты у которых поле value не равняется 'b' и не является строкой
-        //                             dimentionsDict.push({
-        //                                 name: fromKey,
-        //                                 value: listOfThings
-        //                             })
-        //                         } else {}
-        //                     })
-        //                 }
-        //             } else {}
-        //         })
-
-
-
-        //         //console.log(key, value);
-
-        //     }
-        // });
-
-        // dimentionsList = new Set(dimentionsList)
-        //console.log(dimentionsDict)
-
         var dimention_block = document.getElementById('dimention_block')
             //console.log(dimentions)
         try {
@@ -275,26 +202,29 @@ function getGraphs() {
                 "translate(" + margin.left + "," + margin.top + ")");
 
         function filteredData(countries) {
-            console.log(countries)
+
+            //console.log(countries)
 
             dataFilteredCountry = data.filter(function(e) {
-                if (countries.includes(e.geoAreaCode)) {} else {}
-                return e
+                //console.log(countries)
+                if (countries.includes(e.geoAreaCode)) { return e } else {}
             })
+
             console.log(dataFilteredCountry)
-                //тут треба зібрати актуальні значення параметрів, тобто прочитати значення селекторів та відфільтрувати data
+
+            //тут треба зібрати актуальні значення параметрів, тобто прочитати значення селекторів та відфільтрувати data
             actualParameters = []
             dimentionsDict.forEach(function(d) {
-                ids = d.name.toString()
-                cids = ids.replace(/\s/g, '')
-                    //console.log('#' + cids)
-                    //console.log(d3.select('#' + cids).property("value"))
-                actualParameters.push({
-                    name: d.name,
-                    value: d3.select('#' + cids).property("value")
+                    ids = d.name.toString()
+                    cids = ids.replace(/\s/g, '')
+                        //console.log('#' + cids)
+                        //console.log(d3.select('#' + cids).property("value"))
+                    actualParameters.push({
+                        name: d.name,
+                        value: d3.select('#' + cids).property("value")
+                    })
                 })
-            })
-            console.log(actualParameters)
+                //console.log(actualParameters)
 
             dataFiltered = dataFilteredCountry.filter(function(e) {
                 selectorOne = []
@@ -315,7 +245,7 @@ function getGraphs() {
 
             });
 
-            //console.log(dataFiltered)
+            console.log(dataFiltered)
 
 
             //dimentionsDict = dimentionsDict.filter(function(e) { return e.name !== key; });
@@ -330,7 +260,7 @@ function getGraphs() {
 
             // Add X axis --> it is a date format
             var x = d3.scaleTime()
-                .domain(d3.extent(data, function(d) { return d.year; }))
+                .domain(d3.extent(dataFiltered, function(d) { return d.year; }))
                 .range([0, width]);
             svg.append("g")
                 .attr("transform", "translate(0," + height + ")")
@@ -338,7 +268,7 @@ function getGraphs() {
 
             // Add Y axis
             var y = d3.scaleLinear()
-                .domain([0, d3.max(data, function(d) { return +d.value; })])
+                .domain([0, d3.max(dataFiltered, function(d) { return +d.value; })])
                 .range([height, 0]);
             svg.append("g")
                 .call(d3.axisLeft(y));
@@ -364,65 +294,6 @@ function getGraphs() {
                         (d.values)
                 })
 
-            // var margin = { top: 10, right: 30, bottom: 30, left: 10 },
-            //     width = d3.select("#line_block").node().getBoundingClientRect().width - margin.left - margin.right,
-            //     height = 400 - margin.top - margin.bottom;
-
-            // append the svg object to the body of the page
-            // Add an svg element for each group. The will be one beside each other and will go on the next row when no more room available
-            // var svg = d3.select("#line_block")
-            //     .selectAll("uniqueChart")
-            //     .data(sumstat)
-            //     .enter()
-            //     .append("svg")
-            //     .attr("width", width + margin.left + margin.right)
-            //     .attr("height", height + margin.top + margin.bottom)
-            //     .append("g")
-            //     .attr("transform",
-            //         "translate(" + margin.left + "," + margin.top + ")");
-
-            // Add X axis --> it is a date format
-            // var x = d3.scaleTime()
-            //     .domain(d3.extent(data, function(d) { return d.year; }))
-            //     .range([0, width]);
-            // svg
-            //     .append("g")
-            //     .attr("transform", "translate(0," + height + ")")
-            //     .call(d3.axisBottom(x).ticks(3));
-
-            // //Add Y axis
-            // var y = d3.scaleLinear()
-            //     .domain([0, d3.max(data, function(d) { return +d.value; })])
-            //     .range([height, 0]);
-            // svg.append("g")
-            //     .call(d3.axisLeft(y).ticks(5));
-
-            // // color palette
-            // var color = d3.scaleOrdinal()
-            //     .domain(allKeys)
-            //     .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
-
-            // // Draw the line
-            // svg
-            //     .append("path")
-            //     .attr("fill", "none")
-            //     .attr("stroke", function(d) { return color(d.key) })
-            //     .attr("stroke-width", 1.9)
-            //     .attr("d", function(d) {
-            //         return d3.line()
-            //             .x(function(d) { return x(d.year); })
-            //             .y(function(d) { return y(+d.value); })
-            //             (d.values)
-            //     })
-
-            // // Add titles
-            // svg
-            //     .append("text")
-            //     .attr("text-anchor", "start")
-            //     .attr("y", -5)
-            //     .attr("x", 0)
-            //     .text(function(d) { return (d.key) })
-            //     .style("fill", function(d) { return color(d.key) })
         }
 
         //тут треба додати слухачі на всі селектори із параметрами для оновлення функції
