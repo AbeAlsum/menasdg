9f78b45330dffb2119eb6881cfa2077a518fbdfc
HubashovD <34233868+HubashovD@users.noreply.github.com>
2022-07-26 17:45:11 +0300

    Update getMapR2.js

--- web/js/getMapR2.js
+++ web/js/getMapR2.js
@@ -164,6 +164,7 @@ function getMap(text) {
             item = document.getElementById(element.country.replace(/\s/g, '') + "Map")
                 // console.log(item)
             item.style.fill = map_colorScale(element.value)
+            item.style.cursor = 'pointer'
                 // item.style.cssText = 'cursor: pointer; fill: ' + map_colorScale(element.value) + ';'
             item.__data__.properties.value = element.value
 
